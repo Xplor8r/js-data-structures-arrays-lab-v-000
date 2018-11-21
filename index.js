1bf157828d298f767ef9698ca7628f2d67889c67
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-21 01:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendDriver(name) {
-  return drivers.push(name);
-
+function destructivelyAppendDriver(kitten) {
+  return kittens.push(kitten);
 }
+function destructivelyPrependDriver(kitten) {
+  return kittens.unshift(kitten);
+}
+
+function destructivelyRemoveLastDriver(Drivers) {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstDriver(Drivers) {
+  return kittens.shift();
+}
+function appendDriver(name){
+  return [...kittens, name]
+}
+
+function prependDriver(name){
+  return [name, ...kittens]
+}
+
+function removeLastDriver(Driver) {
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+function removeFirstDriver(Driver) {
+  var newArray = kittens.slice(1);
+  return newArray;
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
